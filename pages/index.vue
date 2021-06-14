adeeb7c1795102b2ca422fe1fd97e4dd4b66283b
yanuaraditia <yanuaraditia@outlook.com>
2021-06-14 15:43:22 +0700

    Fxer

--- pages/index.vue
+++ pages/index.vue
@@ -54,18 +54,77 @@
               <img src="https://www.shipbob.com/wp-content/uploads/2019/11/home-sec1-2.svg" alt="Store" data-rjs="2">
             </div>
             <h3>2. Store</h3>
-            <p><span style="font-weight: 400;">We store your inventory in any combination of our fulfillment centers.</span></p>
+            <p><span>We store your inventory in any combination of our fulfillment centers.</span></p>
           </div>
           <div class="col-md-4 text-center">
             <div class="img-wrap">
               <img src="https://www.shipbob.com/wp-content/uploads/2019/11/home-sec1-3.svg" alt="Ship" data-rjs="2">
             </div>
             <h3>3. Ship</h3>
-            <p><span style="font-weight: 400;">As soon as a customer places an order, we ship it from the nearest fulfillment center.</span></p>
+            <p><span>As soon as a customer places an order, we ship it from the nearest fulfillment center.</span></p>
           </div>
         </div>
         <div class="text-center mt-5">
           <a href="https://www.shipbob.com/product/" target="_self" class="btn btn-primary btn-lg is-rounded">How it Works</a>
+        </div>
+      </div>
+    </section>
+    <section class="front-banner-3">
+      <div class="container">
+        <div class="sb_header">
+          <h6>ECOMMERCE FULFILLMENT SERVICES</h6>
+          <h2>ShipBob is an end-to-end fulfillment provider trusted by thousands of DTC brands</h2>
+        </div>
+        <div class="sb_home--sec3-item1 sb_criss-cross --reverse">
+          <div class="sb_criss-cross--text">
+            <h2>Powerful network of fulfillment centers</h2>
+            <p><span>We help you determine optimal inventory placement across our growing fulfillment network to improve delivery speeds and reduce shipping costs. You can scale efficiently by distributing inventory into additional warehouses over time.</span></p>
+            <ul class="list-check">
+              <li><span>Affordable 2-day shipping</span></li>
+              <li><span>Easy-to-understand pricing&nbsp;</span></li>
+              <li><span>Wide range of fulfillment services</span></li>
+              <li><span>Transparency of performance for accountability&nbsp;</span></li>
+              <li><span>Hands-off fulfillment so you can focus on everything else</span></li>
+            </ul>
+            <span class="line mt-5 mb-4"></span>
+            <div class="shipping-providers">
+              <ul>
+                <li>
+                  <img src="https://www.shipbob.com/wp-content/uploads/2019/11/logo-ups.svg" alt="UPS" data-rjs="2">
+                </li>
+                <li>
+                  <img src="https://www.shipbob.com/wp-content/uploads/2019/11/logo-fedex.svg" alt="FedEx" data-rjs="2">
+                </li>
+                <li>
+                  <img src="https://www.shipbob.com/wp-content/uploads/2019/11/logo-dhl.svg" alt="DHL" data-rjs="2">
+                </li>
+                <li>
+                  <img src="https://www.shipbob.com/wp-content/uploads/2019/11/logo-usps.svg" alt="USPS" data-rjs="2">
+                </li>
+              </ul>
+              <p>We ship to anywhere in the world that the carriers deliver.</p>
+            </div>
+          </div>
+          <div class="sb_criss-cross--image">
+            <div class="img-wrap">
+              <span class="dotted-box2"></span>
+              <img src="https://www.shipbob.com/wp-content/uploads/2019/11/home-sec2-1@2x.jpg" alt="Warehouse" data-rjs="2" width="460" height="580">
+            </div>
+          </div>
+        </div>
+        <div class="sb_home--sec3-item2 sb_criss-cross">
+          <div class="sb_criss-cross--text pl-0 pr-lg-5">
+            <h2>Ecommerce fulfillment software</h2>
+            <p><span>KiriminAja’s web-based software acts as your back office for logistics, letting you manage and edit orders, track real-time stock levels across our fulfillment centers, and receive notifications when you need to reorder inventory — all at no extra cost for unlimited users.</span></p>
+            <p><span>We are a tech-driven 3PL, and our powerful tools and algorithms help automate fulfillment and improve your bottom line. Our built-in reports and analytics provide insights to empower you to make better decisions across the supply chain and give you a competitive advantage.&nbsp;</span></p>
+            <p><a href="http://www.shipbob.com/product" target="_self" class="btn btn-light2 is-rounded">Learn More</a></p>
+          </div>
+          <div class="sb_criss-cross--image">
+            <div class="img-wrap">
+              <span class="dotted-box"></span>
+              <img src="https://www.shipbob.com/wp-content/uploads/2019/11/home-sec2-2@2x.png" alt="Laptop" data-rjs="2" width="608" height="560">
+            </div>
+          </div>
         </div>
       </div>
     </section>
